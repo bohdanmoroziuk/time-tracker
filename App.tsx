8b09510e2794b8662fbbd61ebf3f71b40aab0231
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-09 15:17:31 +0300

    chore: add hardcoded state

--- App.tsx
+++ App.tsx
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react'; 
 import { Text, View, ScrollView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-get-random-values';
+import { nanoid } from 'nanoid';
 
 import EditableTimer from 'src/components/EditableTimer';
 import ToggleableTimerForm from 'src/components/ToggleableTimerForm';
 
 import styles from './App.styles';
 
+export interface Timer {
+  id: string;
+  title: string;
+  project: string;
+  elapsed: number;
+  isRunning: boolean;
+}
+
+export interface State {
+  timers: Timer[];
+}
+
 export default function App() {
+  const [state, setState] = useState<State>({
+    timers: [
+      {
+        id: nanoid(),
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        elapsed: 8986300,
+        isRunning: true,
+      },
+      {
+        id: nanoid(),
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        elapsed: 3890985,
+        isRunning: false,
+      },
+    ],
+  });
+
   return (
     <View style={styles.app}>
       <StatusBar style="auto" />
@@ -15,20 +49,16 @@ export default function App() {
       </View>
       <ScrollView style={styles.timerList}>
         <ToggleableTimerForm isOpen={false} />
-        <EditableTimer
-          id="1"
-          title="Mow the lawn"
-          project="House Chores"
-          elapsed={8986300}
-          isRunning
-        />
-        <EditableTimer
-          id="2"
-          title="Bake squash"
-          project="Kitchen Chores"
-          elapsed={3890985}
-          editFormOpen
-        />
+        {state.timers.map((timer) => (
+          <EditableTimer
+            id={timer.id}
+            key={timer.id}
+            title={timer.title}
+            project={timer.project}
+            elapsed={timer.elapsed}
+            isRunning={timer.isRunning}
+          />
+        ))}
       </ScrollView>
     </View>
   );
