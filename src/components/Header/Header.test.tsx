7027f6b4b82b2f13cb69f71dd427794905cfc53b
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-13 11:49:58 +0300

    test(header): add tests

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react-native';
+
+import Header from './Header';
+
+describe('Header', () => {
+  it('renders successfully', () => {
+    const title = "Timers";
+
+    const { toJSON } = render(<Header title={title} />);
+
+    expect(toJSON()).toMatchSnapshot();
+
+  });
+
+  it('renders correctly', () => {
+    const title = "Timers";
+
+    const { queryByText } = render(<Header title={title} />);
+
+    expect(queryByText(title)).toBeDefined();
+  });
+});
