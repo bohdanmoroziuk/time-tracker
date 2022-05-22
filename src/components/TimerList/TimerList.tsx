c1b1e77f7bd42750fe3bf2dc132ccee3f9d39961
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-22 19:03:57 +0300

    refactor(timer-list): add utility function to render timer

--- src/components/TimerList/TimerList.tsx
+++ src/components/TimerList/TimerList.tsx
@@ -2,25 +2,27 @@ import { FunctionComponent } from 'react';
 import { ScrollView } from 'react-native';
 
 import EditableTimer from 'src/components/EditableTimer';
-import { useTimers } from 'src/contexts/timers';
+import { useTimers, Timer } from 'src/contexts/timers';
 
 import styles from './TimerList.styles';
+
+const renderTimer = (timer: Timer) => (
+  <EditableTimer
+    id={timer.id}
+    key={timer.id}
+    title={timer.title}
+    project={timer.project}
+    elapsed={timer.elapsed}
+    isRunning={timer.isRunning}
+  />
+);
 
 const TimerList: FunctionComponent = () => {
   const { timers } = useTimers();
 
   return (
     <ScrollView style={styles.timerList}>
-      {timers.map((timer) => (
-        <EditableTimer
-          id={timer.id}
-          key={timer.id}
-          title={timer.title}
-          project={timer.project}
-          elapsed={timer.elapsed}
-          isRunning={timer.isRunning}
-        />
-      ))}
+      {timers.map(renderTimer)}
     </ScrollView>
   );
 }
