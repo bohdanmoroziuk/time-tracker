6616e85364c9bff354d2b6bdb20a314893b83a90
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-22 18:39:13 +0300

    refactor(utils): add several utility functions

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,14 +1,22 @@
+export const millisecondsToSeconds = (ms: number) => Math.floor((ms / 1000) % 60);
+
+export const millisecondsToMinutes = (ms: number) => Math.floor((ms / 1000 / 60) % 60);
+
+export const millisecondsToHours = (ms: number) => Math.floor(ms / 1000 / 60 / 60);
+
 export const millisecondsToHuman = (ms: number) => {
-  const seconds = Math.floor((ms / 1000) % 60);
-  const minutes = Math.floor((ms / 1000 / 60) % 60);
-  const hours = Math.floor(ms / 1000 / 60 / 60);
+  const seconds = millisecondsToSeconds(ms)
+  const minutes = millisecondsToMinutes(ms);
+  const hours = millisecondsToHours(ms)
+
+  const normalize = (value: number) => value.toString().padStart(2, '0');
 
   const humanized = [
     hours,
     minutes,
     seconds,
   ]
-    .map((value) => value.toString().padStart(2, '0'))
+    .map(normalize)
     .join(':');
 
   return humanized;
